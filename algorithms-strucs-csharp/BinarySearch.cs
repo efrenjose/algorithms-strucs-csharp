73f174fb03a30af7c1d2257ed6aeb470a6386249
Efren Rodriguez <efrenrodriguez@live.com>
2017-11-18 12:38:20 -0600

    initial set up. Add binary search

---
+++ algorithms-strucs-csharp/BinarySearch.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace algorithmsstrucscsharp
+{
+    public class BinarySearch
+    {
+        public int Search(int[] array, int targetValue)
+        {
+            var min = 0;
+            var max = array.Length - 1;
+            var guessLocation = 0;
+
+            while (max >= min)
+            {
+                guessLocation = Convert.ToInt32(Math.Floor(Convert.ToDouble((min + max) / 2)));
+
+                Console.WriteLine("guessLocation: " + guessLocation);
+
+                if (array[guessLocation] == targetValue)
+                {
+                    return guessLocation;
+                }
+
+                if (array[guessLocation] < targetValue)
+                {
+                    min = guessLocation + 1;
+                }
+                else
+                {
+                    max = guessLocation - 1;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ algorithms-strucs-csharp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace algorithmsstrucscsharp
+{
+    public class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            DoBinarySearch();
+        }
+
+        public static void DoBinarySearch()
+        {
+
+            int[] numbers = new int[] {1, 2, 3, 4, 5,
+                                    6, 7, 8, 9, 10,
+                                    11, 12, 13, 14, 15,
+                                    16, 17, 18 ,19, 20};
+
+            BinarySearch binarySearch = new BinarySearch();
+
+            int location = binarySearch.Search(numbers, 7);
+
+            Console.WriteLine("location = " + location);
+        }
+    }
+}
