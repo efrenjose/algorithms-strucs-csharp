67358142ef895bb4dfc2d13fa415fcb5af273c55
Efren Rodriguez <efrenrodriguez@live.com>
2017-11-23 23:12:36 -0600

    add projects for fizzbuzz and prime generator

---
+++ fizzbuzz/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace fizzbuzz
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string output = string.Empty;
+
+            for (int i = 1; i < 100; i++)
+            {
+                if (i % 3 != 0 && i % 5 != 0)
+                {
+                    output += i + ", ";
+                }
+
+                if (i % 3 == 0)
+                {
+                    output += "Fizz,";
+                }
+
+                if (i % 5 == 0)
+                {
+                    output += "Buzz,";
+                }
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}
---
+++ fizzbuzz/prime-generator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace primegenerator
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string output = string.Empty;
+
+            for (int i = 2; i < 100; i++)
+            {
+                for (int j = 2; j <= i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        if (i != j)
+                            break;
+                        output += i + ", ";
+                    }
+                }
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}
